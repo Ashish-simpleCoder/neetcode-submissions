760f0a87acf86769fbe88f9c7b14078a56ee03d9
Ashish Prajapati <62009244+Ashish-simpleCoder@users.noreply.github.com>
2026-07-08 18:59:57 +0530

    Add: rotate-array - submission-2

---
+++ Data Structures & Algorithms/rotate-array/submission-2.ts
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {void} Do not return anything, modify nums in-place instead.
+     */
+    rotate(nums: number[], k: number): void {
+        // Solution 2:- Better version
+        // 1. create cloned_arr
+        // 2. Iterate over all elements from cloned_arr
+        // 2.1 start the replacement from (i+k)%len=(0+2)/5 = 2 position, so it can shift element
+
+        let len = nums.length
+        k = k % len
+
+        const new_arr = [...nums]
+
+        for (let i = 0; i < len; i++) {
+            nums[(i + k) % len] = new_arr[i]
+        }
+    }
+}
+
+//  T(c) = O(n)
+// S(c) = O(n)
